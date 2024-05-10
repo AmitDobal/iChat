390a0823ba29c3e2dd790424d354f7c741248124
Amit Singh Dobal <82360260+AmitDobal@users.noreply.github.com>
2024-05-11 04:05:46 +0530

    Added unread messgaes Realtime only

--- auth-service/controllers/user.controller.js
+++ auth-service/controllers/user.controller.js
@@ -24,6 +24,7 @@ export const updateUserPicUrl = async (req, res) => {
 
 export const updateActiveStatus = async (req, res) => {
   try {
+    
     const { username, activeStatus } = req.body;
     const user = await User.findOne({ username });
     if (!user) return res.status(404).json({ message: "User not found" });
@@ -38,7 +39,7 @@ export const updateActiveStatus = async (req, res) => {
   }
 };
 
-export const serchUser = async (req, res) => {
+export const searchUser = async (req, res) => {
   try {
     const { name } = req.query;
     const users = await User.find({
@@ -51,6 +52,16 @@ export const serchUser = async (req, res) => {
     res.status(200).json(users);
   } catch (error) {
     console.log("Error while serching the user: ", error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
+export const getUserByUsername = async (req, res) => {
+  try {
+    const { username } = req.params;
+    const user = await User.findOne({ username }).select("-password");
+    res.status(200).json(user);
+  } catch (error) {
+    console.log("Error while getUserByUsername: ", error.message);
     res.status(500).json({ error: error.message });
   }
 };
